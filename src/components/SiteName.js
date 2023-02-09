31c3a01b3fc672561093f665b51c18a26add7d17
ChandraG <115159316+Cgancg@users.noreply.github.com>
2023-02-09 17:26:30 +0000

    Axios -call API

--- src/components/SiteName.js
+++ src/components/SiteName.js
@@ -1,9 +1,22 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 export const SiteName = () => {
+  const [quotes, Setquotes] = useState("");
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get(`https://api.quotable.io/random`);
+      // console.log(data.content);
+      Setquotes(data.content);
+    };
+    fetchData();
+  }, []);
   return (
     <div className="SiteName">
       <header className="App-header">
         e-books-cart
-        <p>Helping Little minds read</p>
+        {/* <p>Helping Little minds read</p> */}
+        {quotes && <p>{quotes}</p>}
       </header>
     </div>
   );
